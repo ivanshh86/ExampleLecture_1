cf227d7bbc5625b45f6bdc9eb23232f8346a9527
Ivanshh86 <ivanshh@yandex.ru>
2023-06-09 23:22:20 +0700

    GoodMasha

---
+++ 1.5/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write ("Enter you name: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "masha")
+{
+    Console.WriteLine ("Good! Hi, Masha");
+}
+else
+{
+    Console.WriteLine ("Hello, " + username);
+}
