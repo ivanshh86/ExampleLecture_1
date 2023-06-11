8d3ace57c60927a5135259110003b094b6fecd3e
Ivanshh86 <ivanshh@yandex.ru>
2023-06-11 23:21:50 +0700

    addArray

--- Lesson2/Array/Program.cs
+++ Lesson2/Array/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+int[] array {11, 21, 31, 41, 15, 61, 17, 18, 19};
+array[0] = 12;
+Console.WriteLine(array[0]);
+
+//int max = Max(
+//    Max(a1, b1, c1),
+//    Max(a2, b2, c2),
+//    Max(a3, b3, c3));
+//
+//Console.WriteLine(max);
+
