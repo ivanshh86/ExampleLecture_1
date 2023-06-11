994db3636d815d9ce963bcb9ecfc1f238a81ef16
Ivanshh86 <ivanshh@yandex.ru>
2023-06-11 17:59:15 +0700

    1

--- 1.3/Program.cs
+++ 1.3/Program.cs
@@ -1,11 +1,9 @@
-﻿using System;
+﻿int numberA = new Random().Next(1, 10);   // 1,2,3,4.....9
+Console.WriteLine(numberA);
 
-int numberA = new Random ().Next (1, 10);   // 1,2,3,4.....9
-Console.WriteLine (numberA);
-
-int numberB = new Random ().Next (1, 10);   //(x, y) от x до y-1
-Console.WriteLine (numberB);
+int numberB = new Random().Next(1, 10);   //(x, y) от x до y-1
+Console.WriteLine(numberB);
 
 int result = numberA + numberB;
-Console.WriteLine (result);
+Console.WriteLine(result);
 
