544f64bb544a34a0ee8e8eca1814121d599c67e0
Ivanshh86 <ivanshh@yandex.ru>
2023-06-11 16:48:17 +0700

    addComent

--- 1.3/Program.cs
+++ 1.3/Program.cs
@@ -3,7 +3,7 @@
 int numberA = new Random ().Next (1, 10);   // 1,2,3,4.....9
 Console.WriteLine (numberA);
 
-int numberB = new Random ().Next (1, 10);
+int numberB = new Random ().Next (1, 10);   //(x, y) от x до y-1
 Console.WriteLine (numberB);
 
 int result = numberA + numberB;
