cb436af45fa90f925cae500cf137ed579eef0bd4
Ivanshh86 <ivanshh@yandex.ru>
2023-06-12 21:41:31 +0700

    ArrayFinish

--- Lesson2/Array/Program.cs
+++ Lesson2/Array/Program.cs
@@ -1,4 +1,5 @@
-﻿int Max(int arg1, int arg2, int arg3)
+﻿using System;
+int Max(int arg1, int arg2, int arg3)
 {
     int result = arg1;
     if (arg2 > result) result = arg2;
@@ -6,14 +7,15 @@
     return result;
 }
 
-int[] array = {11, 21, 31, 41, 15, 61, 17, 18, 19};
-array[0] = 12;
-Console.WriteLine(array[4]);
+int[] array = {11, 211, 31, 41, 15, 61, 17, 18, 19};
+// array[0] = 12;
+// Console.WriteLine(array[4]);
 
-//int max = Max(
-//    Max(a1, b1, c1),
-//    Max(a2, b2, c2),
-//    Max(a3, b3, c3));
-//
-//Console.WriteLine(max);
+int result = Max(
+    Max(array[0], array[1], array[2]),
+    Max(array[3], array[4], array[5]),
+    Max(array[6], array[7], array[8])
+    );
+
+Console.WriteLine(result);
 
